517c86717d75d8da148b18489923189a02fb784f
AlvaroMarraes <amarare@upv.edu.es>
2025-09-30 10:30:57 +0200

    modificaciones

--- src/servidor/Logica.js
+++ src/servidor/Logica.js
@@ -1,34 +1,33 @@
 class Logica {
-    constructor(db) {
-        this.db = db;
-    }
+  constructor(db) {
+    this.db = db;
+  }
 
-    guardarMedicion(datos) {
-        return new Promise((resolve, reject) => {
-            const sql = "INSERT INTO Mediciones (sensorID, valor, tiempo) VALUES (?, ?, ?)";
-            this.db.run(sql, [datos.sensorId, datos.valor, datos.tiempo], function (err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve({ ok: true, mensaje: "Medición guardada correctamente", id: this.lastID });
-                }
-            });
-        });
-    }
+  guardarMedicion(datos) {
+    return new Promise((resolve, reject) => {
+      const sql = "INSERT INTO mediciones (sensorId, valor, tiempo) VALUES (?, ?, ?)";
+      this.db.run(sql, [datos.sensorId, datos.valor, datos.tiempo], function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ ok: true, mensaje: "Medición guardada correctamente", id: this.lastID });
+        }
+      });
+    });
+  }
 
-    getMedicion(sensorId) {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM mediciones WHERE sensorId = ? ORDER BY tiempo DESC LIMIT 1";
-            this.db.get(sql, [sensorId], (err, row) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(row || null);
-                }
-            });
-        });
-    }
+  getMedicion(sensorId) {
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT * FROM mediciones WHERE sensorId = ? ORDER BY tiempo DESC LIMIT 1";
+      this.db.get(sql, [sensorId], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row || null);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Logica;
-
